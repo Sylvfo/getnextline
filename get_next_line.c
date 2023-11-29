cc55ea7f7be9109b35036d02d35960e7a1955ea8
Sylvie Forster <sforster@c1r7s4.42lausanne.ch>
2023-11-29 18:45:08 +0100

    debutgetnextline

---
+++ get_next_line.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sforster <sforster@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 17:55:31 by sforster          #+#    #+#             */
+/*   Updated: 2023/11/29 18:28:14 by sforster         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*get_next_line(int fd)
+{
+	char *line;
+	char *nextline;
+
+	argc argv
+
+	if (!... )
+		return (NULL);
+	while (ce qui est lu != \n)
+		line = ce qui est lu. 
+	 
+	line->next = nextline;
+	// line + \n
+
+
+}
